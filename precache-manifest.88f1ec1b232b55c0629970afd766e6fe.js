334fc9d1c8c170a2904de3d258de0506148d724a
sandipsaha597 <40121135+sandipsaha597@users.noreply.github.com>
2020-04-07 16:48:24 +0530

    Add files via upload

---
+++ precache-manifest.88f1ec1b232b55c0629970afd766e6fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "505a64bd3b862f32040c2092da780199",
+    "url": "/todo-app-reactJS/index.html"
+  },
+  {
+    "revision": "3491edf4038f3caf223b",
+    "url": "/todo-app-reactJS/static/css/main.6d27dff9.chunk.css"
+  },
+  {
+    "revision": "e38dda5b4300db8807bf",
+    "url": "/todo-app-reactJS/static/js/2.25ec4e73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app-reactJS/static/js/2.25ec4e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3491edf4038f3caf223b",
+    "url": "/todo-app-reactJS/static/js/main.dd149a4a.chunk.js"
+  },
+  {
+    "revision": "7ec1fd2d30956a816499",
+    "url": "/todo-app-reactJS/static/js/runtime-main.3e97ba75.js"
+  }
+]);
